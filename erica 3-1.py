f3756952719600efc8a6d5dacc0f56fb711734fb
Ericaquisp <velasquezerica41@gmail.com>
2024-08-07 23:53:31 -0300

    Add files via upload

--- erica 3-1.py
+++ erica 3-1.py
@@ -1,15 +1,27 @@
 import random
-
-Aleatorio=random.randint(1,5)
-Intentos=0
-Vidas=6
-Num=0
-
+LI=1 #LI: Limite Inferior
+LS=0 #LS: Limite Superior
+opcion=0
+print("ingrese una opcion 1 para nivel facil y 2 para dificil")
+opcion=int(input())
+if opcion==1:
+    Intentos=0
+    Vidas=6
+    Num=0
+    LS=20
+if opcion==2:
+    Intentos=0
+    Vidas=7
+    Num=0
+    LS=200
+    
+Aleatorio=random.randint(LI,LS)
+  
 print("secreto:" , Aleatorio)
 
 while(Intentos < Vidas):
     Intentos=Intentos+1
-    print("ingrese un numero entre el 1 y 5 usted tendra 6 intentos")
+    print("ingrese un numero entre el" , LI ," y " , LS , "usted tendra" , Vidas , "intentos")
     Num=int(input())
     if Num < Aleatorio:
         print("el numero ingresado es menor, intente nuevamente")
